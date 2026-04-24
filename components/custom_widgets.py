e46f98c82a3d159f7ed983c3977eb71ad7578060
Gavin Dominique <gavindom315@gmail.com>
2026-04-24 02:26:02 -0500

    data table tab

--- components/custom_widgets.py
+++ components/custom_widgets.py
@@ -1,4 +1,6 @@
 
+from PySide6.QtCore import Qt, Signal
+from PySide6.QtGui import QPalette
 from PySide6.QtWidgets import (
     QWidget,
     QMainWindow,
@@ -29,12 +31,52 @@ class MplCanvas(FigureCanvas):
         self.setParent(parent)
 
 class DataWindow(QWidget):
+    file_opened = Signal(str, object)
+    dataframes = {}
+    curr_file = ""
+
     def __init__(self):
         super().__init__()
 
         self.table = QTableWidget()
 
-        self.layout = QVBoxLayout()
+        ## --- File loader sidebar --- ##
+        self.upload_button = QPushButton("Open File")
+        self.upload_button.clicked.connect(self.open_file)
+
+        pixmap = QStyle.StandardPixmap.SP_DialogOpenButton
+        icon = self.style().standardIcon(pixmap)
+        self.upload_button.setIcon(icon)
+
+        self.file_scroller = QScrollArea()
+        self.file_scroller.setWidgetResizable(True)
+        self.file_scroller.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+
+        self.file_container = QWidget()
+        self.flayout = QVBoxLayout(self.file_container)
+        self.file_scroller.setWidget(self.file_container)
+        self.flayout.addStretch()
+
+        self.left_layout = QVBoxLayout()
+        self.left_layout.addWidget(self.upload_button)
+        self.left_layout.addWidget(self.file_scroller)
+
+        self.left_widget = QWidget()
+        self.left_widget.setLayout(self.left_layout)
+        self.left_widget.setMinimumWidth(175)
+        self.left_widget.setMaximumWidth(250)
+
+        # get the table's base color
+        table_bg = self.table.palette().color(QPalette.ColorRole.Base)
+
+        # apply it to the file container
+        self.file_container.setAutoFillBackground(True)
+        palette = self.file_container.palette()
+        palette.setColor(QPalette.ColorRole.Window, table_bg)
+        self.file_container.setPalette(palette)
+
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.left_widget)
         self.layout.addWidget(self.table)
 
         self.setLayout(self.layout)
@@ -63,6 +105,53 @@ class DataWindow(QWidget):
                     self.table.setItem(row_idx, col_idx, QTableWidgetItem("-"))
                 else:
                     self.table.setItem(row_idx, col_idx, QTableWidgetItem(str(value)))
+
+    def open_file(self):
+        file_dialog = QFileDialog()
+        filters = "Data Files (*.csv *.json *.xml *.xlsx);;CSV Files (*.csv);;JSON Files (*.json);;XML Files (*.xml);;Excel Files (*.xlsx)"
+        filepath, _ = file_dialog.getOpenFileName(self, "Open CSV File", "", filters)
+
+        # load the file path based on the type
+        if filepath.endswith(".csv"):
+            data = pd.read_csv(filepath)
+        elif filepath.endswith(".json"):
+            data = pd.read_json(filepath)
+        elif filepath.endswith(".xml"):
+            data = pd.read_xml(filepath)
+        elif filepath.endswith(".xlsx"):
+            data = pd.read_excel(filepath)
+        else:
+            raise ValueError("Unsupported file type")
+
+        # if the data doesn't exist, or it is empty,
+        # then don't bother loading it into the viewer
+        if data is None or data.empty:
+            return
+
+        name = filepath.split("/")[-1]  # grab the filename
+        self.dataframes[name] = data
+        self.add_dataset_button(name)   # add the button
+
+        # set it as the currently open dataset
+        self.set_data(data)
+        self.curr_file = name
+
+        # emit the opened file and dataframe
+        self.file_opened.emit(name, data)
+
+    def add_dataset_button(self, name):
+        btn = QToolButton()
+        btn.setText(name)
+        btn.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        btn.clicked.connect(lambda: self.switch_dataset(name))
+
+        # Insert before the stretch widget
+        self.flayout.insertWidget(self.flayout.count() - 1, btn)
+
+    def switch_dataset(self, name):
+        if name in self.dataframes.keys() and name != self.curr_file:
+            self.curr_file = name
+            self.set_data(self.dataframes[name])
 
 class GraphWindow(QWidget):
     def __init__(self):
--- components/project_page.py
+++ components/project_page.py
@@ -25,16 +25,19 @@ from components.custom_dialogs import SingleFileGraph, DataInformation, MissingV
 from components.custom_widgets import DataWindow, GraphWindow, InfoWindow
 
 class ProjectPage(QMainWindow):
+    dataframes = {}
     def __init__(self):
         super().__init__()
 
         self.dataframes = {}
         self.current_dataframe = ""
-        self.table_maker = None
-        self.data_info = None
+        self.table_dialog = None
+        self.info_dialog = None
 
         ## --- Actual table view of the selected file --- ##
         self.data_tab = DataWindow()
+        self.data_tab.file_opened.connect(self.add_dataframe)
+
         self.graph_tab = GraphWindow()
         self.info_tab = InfoWindow()
 
@@ -42,42 +45,6 @@ class ProjectPage(QMainWindow):
         self.tabs.addTab(self.data_tab, "Data Tables")
         self.tabs.addTab(self.graph_tab, "Graphs")
         self.tabs.addTab(self.info_tab, "Info Docs")
-
-
-        ## --- File loader sidebar --- ##
-        self.upload_button = QPushButton("Open File")
-        self.upload_button.clicked.connect(self.open_file)
-
-        pixmap = QStyle.StandardPixmap.SP_DialogOpenButton
-        icon = self.style().standardIcon(pixmap)
-        self.upload_button.setIcon(icon)
-
-        self.file_scroller = QScrollArea()
-        self.file_scroller.setWidgetResizable(True)
-        self.file_scroller.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-
-        self.file_container = QWidget()
-        self.flayout = QVBoxLayout(self.file_container)
-        self.file_scroller.setWidget(self.file_container)
-        self.flayout.addStretch()
-
-        self.left_layout = QVBoxLayout()
-        self.left_layout.addWidget(self.upload_button)
-        self.left_layout.addWidget(self.file_scroller)
-
-        self.left_widget = QWidget()
-        self.left_widget.setLayout(self.left_layout)
-        self.left_widget.setMinimumWidth(175)
-        self.left_widget.setMaximumWidth(250)
-
-        # get the table's base color
-        table_bg = self.data_viewer.table.palette().color(QPalette.ColorRole.Base)
-
-        # apply it to the file container
-        self.file_container.setAutoFillBackground(True)
-        palette = self.file_container.palette()
-        palette.setColor(QPalette.ColorRole.Window, table_bg)
-        self.file_container.setPalette(palette)
 
         ## --- Plotly visualizer sidebar --- ##
         single_file_graph = QPushButton("+")
@@ -116,13 +83,7 @@ class ProjectPage(QMainWindow):
 
         ## --- Some styling stuff --- ##
         # get the table's base color
-        table_bg = self.data_viewer.table.palette().color(QPalette.ColorRole.Base)
-
-        # apply it to the file container
-        self.file_container.setAutoFillBackground(True)
-        palette = self.file_container.palette()
-        palette.setColor(QPalette.ColorRole.Window, table_bg)
-        self.file_container.setPalette(palette)
+        table_bg = self.data_tab.table.palette().color(QPalette.ColorRole.Base)
 
         # and to the graph container
         self.graph_container.setAutoFillBackground(True)
@@ -132,42 +93,14 @@ class ProjectPage(QMainWindow):
 
         ## ---- Main widget / layout container ---- ##
         self.main_container = QSplitter(Qt.Orientation.Horizontal)
-        self.main_container.addWidget(self.left_widget)
         self.main_container.addWidget(self.tabs)
         self.main_container.addWidget(self.right_widget)
-        self.main_container.setSizes([175, 600, 175])
+        self.main_container.setSizes([775, 175])
 
         self.setCentralWidget(self.main_container)
 
-    def open_file(self):
-        file_dialog = QFileDialog()
-        filters = "Data Files (*.csv *.json *.xml *.xlsx);;CSV Files (*.csv);;JSON Files (*.json);;XML Files (*.xml);;Excel Files (*.xlsx)"
-        filepath, _ = file_dialog.getOpenFileName(self, "Open CSV File", "", filters)
-
-        # load the file path based on the type
-        if filepath.endswith(".csv"):
-            data = pd.read_csv(filepath)
-        elif filepath.endswith(".json"):
-            data = pd.read_json(filepath)
-        elif filepath.endswith(".xml"):
-            data = pd.read_xml(filepath)
-        elif filepath.endswith(".xlsx"):
-            data = pd.read_excel(filepath)
-        else:
-            raise ValueError("Unsupported file type")
-
-        # if the data doesn't exist, or it is empty,
-        # then don't bother loading it into the viewer
-        if data is None or data.empty:
-            return
-
-        name = filepath.split("/")[-1]  # grab the filename
-        self.dataframes[name] = data    # add dataset to dictionary
-        self.add_dataset_button(name)   # add the button
-
-        # set it as the currently open dataset
-        self.current_dataframe = name
-        self.data_viewer.set_data(data)
+    def add_dataframe(self, name, df):
+        self.dataframes[name] = df
 
     def generate_table(self, num: int):
         if num == 1:    # if making a single-file graph
@@ -176,10 +109,10 @@ class ProjectPage(QMainWindow):
                 return
 
             # then open the popup for creating a table
-            self.table_maker = SingleFileGraph(self.dataframes, self)
-            self.table_maker.setModal(True)
-            self.table_maker.finished.connect(self._on_table_maker_closed)
-            self.table_maker.open()
+            self.table_dialog = SingleFileGraph(self.dataframes, self)
+            self.table_dialog.setModal(True)
+            self.table_dialog.finished.connect(self._on_table_dialog_closed)
+            self.table_dialog.open()
 
         if num == 2:  # if making a multi-file graph
             if len(self.dataframes) < 2:  # make sure there are at least two files loaded
@@ -188,8 +121,8 @@ class ProjectPage(QMainWindow):
 
             QMessageBox.information(self, "Work In Progress","No functionality yet.")
 
-    def _on_table_maker_closed(self, _result):
-        self.table_maker = None
+    def _on_table_dialog_closed(self, _result):
+        self.table_dialog = None
 
     def generate_data_info(self, num: int):
         if len(self.dataframes) < 1:  # make sure there is at least one file loaded
@@ -198,31 +131,16 @@ class ProjectPage(QMainWindow):
             return
 
         if num == 1:
-            self.data_info = DataInformation(self.dataframes, self)
-            self.data_info.setModal(True)
-            self.data_info.finished.connect(self._on_data_info_closed)
-            self.data_info.open()
+            self.info_dialog = DataInformation(self.dataframes, self)
+            self.info_dialog.setModal(True)
+            self.info_dialog.finished.connect(self._on_info_dialog_closed)
+            self.info_dialog.open()
 
         elif num == 2:
-            self.data_info = MissingValueAnalysis(self.dataframes, self)
-            self.data_info.setModal(True)
-            self.data_info.finished.connect(self._on_data_info_closed)
-            self.data_info.open()
+            self.info_dialog = MissingValueAnalysis(self.dataframes, self)
+            self.info_dialog.setModal(True)
+            self.info_dialog.finished.connect(self._on_info_dialog_closed)
+            self.info_dialog.open()
 
-    def _on_data_info_closed(self, _result):
-        self.data_info = None
-
-    def add_dataset_button(self, name):
-        btn = QToolButton()
-        btn.setText(name)
-        btn.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
-        btn.clicked.connect(lambda: self.switch_dataset(name))
-
-        # Insert before the stretch widget
-        # self.flayout.addWidget(btn)
-        self.flayout.insertWidget(self.flayout.count() - 1, btn)
-
-    def switch_dataset(self, name):
-        if name in self.dataframes and name != self.current_dataframe:
-            self.current_dataframe = name
-            self.data_viewer.set_data(self.dataframes[name])
+    def _on_info_dialog_closed(self, _result):
+        self.info_dialog = None
